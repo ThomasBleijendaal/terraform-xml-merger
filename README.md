09de2d17831309b952607d54e4575ea81ed1f0ac
Thomas Bleijendaal <t.bleijendaal@wearetriple.com>
2023-08-22 20:43:17 +0200

    Swagger merger

--- README.md
+++ README.md
@@ -1,14 +1,28 @@
 # Apim Terraform
 
-Additions to make cicd with API Management better.
+Additions to make cicd for API Management better.
 
-## Environment variables
+## Xml Merger Terraform Provider
+
+Merges xml policies into bigger files, so xml policies can be composed of fragments,
+without relying on policy fragments.
+
+### Environment variables
 
 - `TF_XML_POLICIES_FOLDER`: Folder to scan for xml fragments.
 - `TF_XML_BASE_POLICY`: Path to base xml file to merge fragments into.
 
-## Variable format
+### Variable format
 
-```%{A-Z_}:{a-z}```: Name of variable (EXAMPLE_VARIABLE) followed by type (bool|int|string): (`%EXAMPLE_VARIABLE:bool%`).
+`%{A-Z_}:{a-z}`: Name of variable (`EXAMPLE_VARIABLE`) followed by type (`bool|int|string`): (`%EXAMPLE_VARIABLE:bool%`).
 
-Variable will be parsed and added to terraform schema.
+Variables will be parsed and added to terraform schema.
+
+## Swagger Merger
+
+Merges multiple swagger json files into single files, so API Management api version sets
+can be composed of multiple backends.
+
+### Request format
+
+```/json?url=http://absolute-swagger.url/1&url=http://absolute-swagger.url/2```
---
+++ src/OpenApiMerger/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi.Readers;
+using Microsoft.OpenApi.Writers;
+using OpenApiMerger.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpClient();
+
+var app = builder.Build();
+
+app.Map("/json", async ([FromQuery] string[] url, [FromServices] HttpClient httpClient, CancellationToken cancellationToken) =>
+{
+    var urls = url.Select(url => Uri.TryCreate(url, UriKind.Absolute, out var uri) ? uri : null).OfType<Uri>().ToArray();
+
+    var rootDocument = new OpenApiDocument
+    {
+        Info = new(),
+        Paths = new(),
+        Components = new()
+    };
+
+    var documents = await Task.WhenAll(urls.Select(async uri =>
+    {
+        var jsonStream = await httpClient.GetStreamAsync(uri.ToString(), cancellationToken);
+
+        var reader = new OpenApiStreamReader();
+
+        // Act
+        var schema = reader.Read(jsonStream, out var diagnostic);
+
+        return schema;
+    }));
+
+    foreach (var document in documents)
+    {
+        rootDocument.Servers.AddRange(document.Servers);
+
+        rootDocument.Paths.AddOrSetRange(document.Paths);
+
+        rootDocument.Components.Schemas.AddOrSetRange(document.Components.Schemas);
+
+        rootDocument.Components.SecuritySchemes.AddOrSetRange(document.Components.SecuritySchemes);
+    }
+
+    var textWriter = new StringWriter();
+    var writer = new OpenApiJsonWriter(textWriter);
+
+    rootDocument.SerializeAsV3(writer);
+
+    return Results.Text(textWriter.ToString(), "application/json");
+});
+
+app.Run();
